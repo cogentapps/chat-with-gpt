ff9f3517ec01ede9e1b790cf6fbc7e4fa603d5a1
ZauberNerd <zaubernerd@zaubernerd.de>
2023-07-07 15:21:20 +0200

    Replace Node.js's Buffer with native Typed Arrays on the client-side
    Vite does not natively ship a Buffer polyfill and most of the
    functionality that is required here, can be implemented natively (except
    for the byte-wise compare, for that I had to write my own function).

--- app/src/core/tokenizer/bpe.ts
+++ app/src/core/tokenizer/bpe.ts
@@ -1,6 +1,11 @@
+import { compareUint8Array } from "../utils";
+
 const MAX_NUM_THREADS = 128;
 
 type MergeRange = { start: number, end: number };
+
+const textDecoder = new TextDecoder();
+const textEncoder = new TextEncoder();
 
 export class RankMap {
     private values = new Map<string, number>();
@@ -14,23 +19,23 @@ export class RankMap {
     }
 
     public set(bytes: Uint8Array, rank: number) {
-        const key = Buffer.from(bytes).toString();
+        const key = textDecoder.decode(bytes);
         this.values.set(key, rank);
     }
 
     public get(bytes: Uint8Array) {
-        const key = Buffer.from(bytes).toString();
+        const key = textDecoder.decode(bytes);
         return this.values.get(key);
     }
 
     public keys() {
-        return Array.from(this.values.keys()).map(k => Buffer.from(k));
+        return Array.from(this.values.keys()).map(k => textEncoder.encode(k));
     }
 
     public inverted() {
         const inverted = new Map<number, Uint8Array>();
         for (const [key, value] of Array.from(this.values.entries())) {
-            inverted.set(value, new Uint8Array(Buffer.from(key)));
+            inverted.set(value, textEncoder.encode(key));
         }
         return inverted;
     }
@@ -100,10 +105,10 @@ export class CoreBPE {
         const decoder: Map<number, Uint8Array> = encoder.inverted();
 
         const specialTokensDecoder: Map<number, Uint8Array> = new Map(
-            Array.from(specialTokensEncoder.entries()).map(([k, v]) => [v, new Uint8Array(Buffer.from(k))])
+            Array.from(specialTokensEncoder.entries()).map(([k, v]) => [v, textEncoder.encode(k)])
         );
         const sortedTokenBytes: Uint8Array[] = Array.from(encoder.keys());
-        sortedTokenBytes.sort((a, b) => Buffer.compare(a, b));
+        sortedTokenBytes.sort((a, b) => compareUint8Array(a, b));
 
         this.encoder = encoder;
         this.specialTokensEncoder = specialTokensEncoder;
@@ -136,7 +141,7 @@ export class CoreBPE {
         const ret: number[] = [];
         let match: RegExpExecArray | null;
         while ((match = regex.exec(text)) !== null) {
-            const piece = new Uint8Array(Buffer.from(match[0]));
+            const piece = textEncoder.encode(match[0]);
             const token = this.encoder.get(piece);
             if (token !== undefined) {
                 ret.push(token);
@@ -167,7 +172,7 @@ export class CoreBPE {
             const end = nextSpecial === null ? text.length : nextSpecial.index;
             let match: RegExpExecArray | null;
             while ((match = regex.exec(text.slice(start, end))) !== null) {
-                const piece = new Uint8Array(Buffer.from(match[0]));
+                const piece = textEncoder.encode(match[0]);
                 const token = this.encoder.get(piece);
                 if (token !== undefined) {
                     lastPieceTokenLen = 1;
@@ -208,7 +213,7 @@ export class CoreBPE {
         if (token !== undefined) {
             return token;
         }
-        const pieceStr = Buffer.from(piece).toString("utf-8");
+        const pieceStr = textDecoder.decode(piece);
         if (this.specialTokensEncoder.has(pieceStr)) {
             return this.specialTokensEncoder.get(pieceStr)!;
         }
