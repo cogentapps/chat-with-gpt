9bdfb432e7426e98ee138da93ba40833fcf5b64e
Rían Errity <rian@paradaux.io>
2023-11-29 14:42:37 +0000

    Update the system prompt to not mention 2021 so as not to limit gpt-4-turbo

--- app/src/plugins/system-prompt.tsx
+++ app/src/plugins/system-prompt.tsx
@@ -5,8 +5,6 @@ import { OpenAIMessage, Parameters } from "../core/chat/types";
 
 export const defaultSystemPrompt = `
 You are ChatGPT, a large language model trained by OpenAI.
-Knowledge cutoff: 2021-09
-Current date and time: {{ datetime }}
 `.trim();
 
 export interface SystemPromptPluginOptions {
