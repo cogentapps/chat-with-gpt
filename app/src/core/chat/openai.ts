b8b6bdcc7fb7f3f3257386a959e7ad0b0f9ff695
Markus Vuorio <markus.vuorio@iki.fi>
2024-01-31 08:21:52 +0200

    Add max tokens

--- app/src/core/chat/openai.ts
+++ app/src/core/chat/openai.ts
@@ -145,6 +145,7 @@ export const maxTokensByModel = {
     "gpt-4-0613": 8192,
     "gpt-4-32k": 32768,
     "gpt-4-32k-0613": 32768,
+    "gpt-4-1106-preview": 8192,
     "gpt-3.5-turbo-16k": 16384,
     "gpt-3.5-turbo-0613": 4096,
     "gpt-3.5-turbo-16k-0613": 16384,
